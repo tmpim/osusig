ea3b287078a07e9680d0001315b236dcbe7a4bd2
Lemmmy <lemonplays@gmail.com>
2015-10-03 21:10:44 +0100

    Added orange, black, and (hopefully) actually fixed caching now

--- class/generator.php
+++ class/generator.php
@@ -148,9 +148,10 @@ function imagecreatefrombmp( $filename )
 }
 
 header("Content-Type: image/png");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 $template = imagecreatefrompng($templateDirectory . $colour . ".png");
 imageAlphaBlending($template, true);
