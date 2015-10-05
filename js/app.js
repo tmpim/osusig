29e65bb8c1c4e0cc01643ca854b7965cdd4c5610
Jerry <jerryxu23@gmail.com>
2015-10-05 13:50:50 -0700

    Weird trailing spaces, enter, and auto-select

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ $(function() {
     var colour = "pink";
     var mode = 0;
     var ppmode = -1;
-    
+
     var lc = 0;
-    
+
     function reloadSig() {
         if (new Date().getTime() >= lc + 500) {
             lc = new Date().getTime();
-            
+
             var url = "sig.php?";
-            
+
             url += "colour=" + colour;
             url += "&uname=" + encodeURIComponent(($("input[name=uname]").val() || "Lemmmy"));
             url += "&mode=" + mode;
             url += (ppmode >= 0 ? "&pp=" + ppmode : "");
-            
+
             url += ($("input[name=adv-av-margin]").prop('checked') ? "&removeavmargin" : "");
             url += ($("input[name=adv-flag-shadow]").prop('checked') ? "&flagshadow" : "");
             url += ($("input[name=adv-flag-stroke]").prop('checked') ? "&flagstroke" : "");
@@ -25,38 +25,51 @@ $(function() {
             $("img.preview").remove();
 
             var newimg = $("<img />", {
-                class: "preview lazy", 
-                width: 338, 
-                height: 94, 
-                src: url 
+                class: "preview lazy",
+                width: 338,
+                height: 94,
+                src: url
             });
 
             $("#previewarea").append(newimg);
 
-            $("input[name=out]").val("[img]" + fullurl + "[/img]"); 
+            $("input[name=out]").val("[img]" + fullurl + "[/img]");
         }
     }
-    
+
     $("#regen").click(reloadSig);
-    
+
+    $("#uname").keypress(function(e) {
+        if(e.which === 13) {
+            e.preventDefault();
+            e.stopPropagation();
+            reloadSig();
+            this.select();
+        }
+    });
+
+    $("#out").click(function() {
+        this.select();
+    });
+
     $(".colours li").each(function() {
         $(this).click(function() {
             $(".colours li").each(function() {
                  $(this).removeClass("selected");
             });
             $(this).addClass("selected");
-            
+
             colour = $(this).attr('id').replace("colour-", "");
         });
     });
-    
+
     $(".modes li").each(function() {
         $(this).click(function() {
             $(".modes li").each(function() {
                  $(this).removeClass("selected");
             });
             $(this).addClass("selected");
-            
+
             if ($(this).hasClass("osu")) {
                 mode = 0;
             } else if ($(this).hasClass("taiko")) {
@@ -68,15 +81,17 @@ $(function() {
             }
         });
     });
-    
+
     $(".ppmodes li").each(function() {
         $(this).click(function() {
             $(".ppmodes li").each(function() {
                  $(this).removeClass("selected");
             });
             $(this).addClass("selected");
-            
+
             ppmode = $(this).attr('id').replace("ppmode-", "");
         });
     });
+
+    $("#uname").focus();
 });
--- sig.php
+++ sig.php
@@ -47,18 +47,18 @@ switch ($mode) {
 }
 
 $userInfo = $mc->get("stats_" . strtolower($uname));
-    
+
 if (!$userInfo) {
     $userInfo = json_decode(file_get_contents($apiURL . 'get_user' . '?' .
                               'k'       . '=' . constant('AKEY') .
                               '&u'       . '=' . $uname .
                               '&m'       . '=' . $mode))[0];
-    
+
     $mc->set("stats_" . strtolower($uname), $userInfo, 180);
 }
 
-function getFontSize ($size) { 
-    return ($size * 3) / 4; 
+function getFontSize ($size) {
+    return ($size * 3) / 4;
 }
 
 $img = new Imagick($templateDirectory . $colour . '.png');
@@ -76,7 +76,7 @@ header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 // rank
 $draw->setFont($fontRegular);
 $draw->setFontSize(14);
-$draw->setFillColor($textWhite); 
+$draw->setFillColor($textWhite);
 $draw->setTextAlignment(\Imagick::ALIGN_RIGHT);
 
 $rankTextWidth = $img->queryFontMetrics($draw, '#' . number_format($userInfo->pp_rank))['textWidth'];
@@ -123,7 +123,7 @@ $nameTextWidth = abs($nameDimensions[4] - $nameDimensions[0]);
 while ($nameTextWidth > 198 - $rankTextWidth) {
     $nameDimensions = imagettfbbox(getFontSize($nameFontSize), 0, $fontMedium, $userInfo->username);
     $nameTextWidth = abs($nameDimensions[4] - $nameDimensions[0]);
-    
+
     $nameFontSize -= 0.5;
 }
 
@@ -137,7 +137,7 @@ $draw->setFont($fontRegular);
 if (isset($_GET['pp']) && $pp == 2) {
     $draw->setFontSize(10);
     $draw->setTextAlignment(\Imagick::ALIGN_RIGHT);
-    
+
     $ppText = number_format(floor($userInfo->pp_raw)) . 'pp';
     $img->annotateImage($draw, 326, 17, 0, $ppText);
 }
@@ -145,7 +145,7 @@ if (isset($_GET['pp']) && $pp == 2) {
 // accuracy & play count
 
 $draw->setFontSize(14);
-$draw->setFillColor($textGrey); 
+$draw->setFillColor($textGrey);
 $draw->setTextAlignment(\Imagick::ALIGN_LEFT);
 
 $img->annotateImage($draw, 91, 56, 0, 'Accuracy');
@@ -177,7 +177,7 @@ if (!$cachedPicture) {
     $avatar->setImageFormat('png');
 }
 
-$avatar->setImageMatte(1); 
+$avatar->setImageMatte(1);
 
 $avatarSize = isset($_GET['removeavmargin']) ? 80 : 76;
 $avatarPos = isset($_GET['removeavmargin']) ? 7 : 9;
